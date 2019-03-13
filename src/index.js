3594c112a19d85a5da18dabc0c08959aa7212c07
Artsiom Rak <ra44elo@gmail.com>
2019-03-13 19:16:49 +0300

    feat: add implementation of the function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let trianglesNumber = 0;
+  preferences.unshift(0);
+  for(let firstSpichonee = 1; firstSpichonee < preferences.length; firstSpichonee++) {
+    secondSpichonee = preferences[firstSpichonee];
+    thirdSpichonee = preferences[secondSpichonee];
+    if(preferences[thirdSpichonee] == firstSpichonee) {
+      trianglesNumber ++;
+    }
+  }
+  return Math.floor(trianglesNumber / 3);
 };
